4f6f8d2c308f4f4c9faf1a9d4ffdac3a1db81aba
dongbab <fordryu@gmail.com>
2023-05-03 19:36:56 +0900

    edit community write

--- pages/community/write.tsx
+++ pages/community/write.tsx
@@ -5,11 +5,11 @@ const Write: NextPage = () => {
     <Layout hasTabBar canGoBack title="1:1 문의 작성">
       <form className="px-4 py-10">
         <textarea
-          className="mt-1 shadow-sm w-full focus:ring-orange-500 rounded-md border-gray-300 focus:border-orange-500 "
+          className="mt-1 shadow-sm w-full focus:ring-blue-500 rounded-md border-gray-300 focus:border-blue-500 "
           rows={4}
-          placeholder="Ask a question!"
+          placeholder="문의하실 내용을 적어주세요."
         />
-        <button className="mt-2 w-full bg-orange-500 hover:bg-orange-600 text-white py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium focus:ring-2 focus:ring-offset-2 focus:ring-orange-500 focus:outline-none ">
+        <button className="mt-2 w-full bg-blue-500 hover:bg-blue-600 text-white py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium focus:ring-2 focus:ring-offset-2 focus:ring-blue-500 focus:outline-none ">
           Submit
         </button>
       </form>
