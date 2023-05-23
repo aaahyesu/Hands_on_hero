faa1a9c6da1ad9cf51f62bcb2060b0ad2aa088c5
Hyesu <skxldzm123@naver.com>
2023-05-23 23:14:34 +0900

    it's me

--- libs/client.ts
+++ libs/client.ts
@@ -1,11 +1,11 @@
 import { PrismaClient } from "@prisma/client";
 
-declare global {
-    var client: PrismaClient | undefined;
-}
+// declare global {
+//   var client: PrismaClient | undefined;
+// }
 
-const client = global.client || new PrismaClient;
+// const client = global.client || new PrismaClient();
 
-if(process.env.NODE_ENV === "development") global.client = client;
+// if (process.env.NODE_ENV === "development") global.client = client;
 
 export default new PrismaClient();
