2c5fd285ebdfb771bcc71ba96507f863d185a3e5
Hyesu <skxldzm123@naver.com>
2023-05-24 19:32:50 +0900

    ui change

---
+++ pages/mypage.tsx
@@ -0,0 +1,188 @@
+import type { NextPage } from "next";
+import Layout from "@/components/layout";
+import Link from "next/link";
+
+const MyPage: NextPage = () => {
+  return (
+    <Layout hasTabBar title="마이페이지">
+      <div className="px-4 py-4">
+        <div className="flex items-center space-x-3 rounded-lg border border-gray-200 px-3 py-3 shadow-sm">
+          <div className="h-12 w-12 rounded-full bg-gray-500" />
+          <div className="flex-col-2 flex px-4">
+            <span className="text-md font-bold text-gray-900">김혜수</span>
+          </div>
+          <div className="flex flex-grow justify-end"></div>
+          <Link href="/profile">
+            <p className="rounded-lg bg-black px-1.5 py-2 text-sm text-white shadow-sm">
+              프로필 보기
+            </p>
+          </Link>
+        </div>
+        <p className="mb-2 mt-6 text-lg font-bold">나의 서비스 관리</p>
+        <div className="flex-col-2 flex py-1">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke-width="1.5"
+            stroke="currentColor"
+            className="h-8 w-8"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L10.582 16.07a4.5 4.5 0 01-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 011.13-1.897l8.932-8.931zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0115.75 21H5.25A2.25 2.25 0 013 18.75V8.25A2.25 2.25 0 015.25 6H10"
+            />
+          </svg>
+          <span className="flex items-center px-3">
+            나의 요청서 관리 (목록/수정)
+          </span>
+        </div>
+        <div className="flex-col-2 flex py-1">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke-width="1.5"
+            stroke="currentColor"
+            className="h-8 w-8"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="M11.48 3.499a.562.562 0 011.04 0l2.125 5.111a.563.563 0 00.475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 00-.182.557l1.285 5.385a.562.562 0 01-.84.61l-4.725-2.885a.563.563 0 00-.586 0L6.982 20.54a.562.562 0 01-.84-.61l1.285-5.386a.562.562 0 00-.182-.557l-4.204-3.602a.563.563 0 01.321-.988l5.518-.442a.563.563 0 00.475-.345L11.48 3.5z"
+            />
+          </svg>
+          <span className="flex items-center px-3">리뷰 관리 (목록/수정)</span>
+        </div>
+        <div className="flex-col-2 flex py-1">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke-width="1.5"
+            stroke="currentColor"
+            className="h-8 w-8"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="M12 9v3.75m-9.303 3.376c-.866 1.5.217 3.374 1.948 3.374h14.71c1.73 0 2.813-1.874 1.948-3.374L13.949 3.378c-.866-1.5-3.032-1.5-3.898 0L2.697 16.126zM12 15.75h.007v.008H12v-.008z"
+            />
+          </svg>
+          <span className="flex items-center px-3">사용자 차단 목록</span>
+        </div>
+        <div className="flex-col-2 flex py-1">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke-width="1.5"
+            stroke="currentColor"
+            className="h-8 w-8"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="M12 6v12m-3-2.818l.879.659c1.171.879 3.07.879 4.242 0 1.172-.879 1.172-2.303 0-3.182C13.536 12.219 12.768 12 12 12c-.725 0-1.45-.22-2.003-.659-1.106-.879-1.106-2.303 0-3.182s2.9-.879 4.006 0l.415.33M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+            />
+          </svg>
+          <span className="flex items-center px-3">가상 머니 충전</span>
+        </div>
+        <div className="flex-col-2 mb-2 flex py-1">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke-width="1.5"
+            stroke="currentColor"
+            className="h-8 w-8"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="M9 12h3.75M9 15h3.75M9 18h3.75m3 .75H18a2.25 2.25 0 002.25-2.25V6.108c0-1.135-.845-2.098-1.976-2.192a48.424 48.424 0 00-1.123-.08m-5.801 0c-.065.21-.1.433-.1.664 0 .414.336.75.75.75h4.5a.75.75 0 00.75-.75 2.25 2.25 0 00-.1-.664m-5.8 0A2.251 2.251 0 0113.5 2.25H15c1.012 0 1.867.668 2.15 1.586m-5.8 0c-.376.023-.75.05-1.124.08C9.095 4.01 8.25 4.973 8.25 6.108V8.25m0 0H4.875c-.621 0-1.125.504-1.125 1.125v11.25c0 .621.504 1.125 1.125 1.125h9.75c.621 0 1.125-.504 1.125-1.125V9.375c0-.621-.504-1.125-1.125-1.125H8.25zM6.75 12h.008v.008H6.75V12zm0 3h.008v.008H6.75V15zm0 3h.008v.008H6.75V18z"
+            />
+          </svg>
+          <span className="flex items-center px-3">결제 내역</span>
+        </div>
+        <div className="border border-gray-200"></div>
+        <div className="py-2 text-lg font-bold">슈퍼맨</div>
+        <div className="flex-col-2 flex py-1">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke-width="1.5"
+            stroke="currentColor"
+            className="h-8 w-8"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="M15.666 3.888A2.25 2.25 0 0013.5 2.25h-3c-1.03 0-1.9.693-2.166 1.638m7.332 0c.055.194.084.4.084.612v0a.75.75 0 01-.75.75H9a.75.75 0 01-.75-.75v0c0-.212.03-.418.084-.612m7.332 0c.646.049 1.288.11 1.927.184 1.1.128 1.907 1.077 1.907 2.185V19.5a2.25 2.25 0 01-2.25 2.25H6.75A2.25 2.25 0 014.5 19.5V6.257c0-1.108.806-2.057 1.907-2.185a48.208 48.208 0 011.927-.184"
+            />
+          </svg>
+          <span className="flex items-center px-3">수락 요청서 목록</span>
+        </div>
+        <div className="flex-col-2 mb-2 flex py-1">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke-width="1.5"
+            stroke="currentColor"
+            className="h-8 w-8"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="M15 9h3.75M15 12h3.75M15 15h3.75M4.5 19.5h15a2.25 2.25 0 002.25-2.25V6.75A2.25 2.25 0 0019.5 4.5h-15a2.25 2.25 0 00-2.25 2.25v10.5A2.25 2.25 0 004.5 19.5zm6-10.125a1.875 1.875 0 11-3.75 0 1.875 1.875 0 013.75 0zm1.294 6.336a6.721 6.721 0 01-3.17.789 6.721 6.721 0 01-3.168-.789 3.376 3.376 0 016.338 0z"
+            />
+          </svg>
+          <span className="flex items-center px-3">자격증 등록</span>
+        </div>
+        <div className="border border-gray-200"></div>
+        <div className="py-2 text-lg font-bold">설정</div>
+        <div className="flex-col-2 mb-2 flex py-1">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke-width="1.5"
+            stroke="currentColor"
+            className="h-8 w-8"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="M15.75 9V5.25A2.25 2.25 0 0013.5 3h-6a2.25 2.25 0 00-2.25 2.25v13.5A2.25 2.25 0 007.5 21h6a2.25 2.25 0 002.25-2.25V15m3 0l3-3m0 0l-3-3m3 3H9"
+            />
+          </svg>
+
+          <span className="flex items-center px-3">로그아웃</span>
+        </div>
+        <div className="flex-col-2 flex">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke-width="1.5"
+            stroke="currentColor"
+            className="h-8 w-8"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="M22 10.5h-6m-2.25-4.125a3.375 3.375 0 11-6.75 0 3.375 3.375 0 016.75 0zM4 19.235v-.11a6.375 6.375 0 0112.75 0v.109A12.318 12.318 0 0110.374 21c-2.331 0-4.512-.645-6.374-1.766z"
+            />
+          </svg>
+
+          <span className="flex items-center px-3">탈퇴하기</span>
+        </div>
+      </div>
+    </Layout>
+  );
+};
+
+export default MyPage;
--- pages/services/[id].tsx
+++ pages/services/[id].tsx
@@ -4,34 +4,41 @@ const ServiceDetail: NextPage = () => {
   return (
     <Layout hasTabBar canGoBack title="요청서 상세내용">
       <div className="px-4 py-4">
-        <div className="mb-8">
-          <div className="h-96 bg-slate-300" />
-          <div className="flex cursor-pointer items-center space-x-3 border-b border-t py-3">
-            <div className="h-12 w-12 rounded-full bg-slate-300" />
-            <div>
-              <p className="text-lg font-medium text-gray-700">김혜수</p>
+        <label className="py-2 text-xl font-bold">요청자 프로필</label>
+        <div className="pt-2">
+          <div className="flex cursor-pointer items-center space-x-3 rounded-lg border border-gray-400 py-3">
+            <div className="px-4">
+              <p className="text-lg font-medium text-black">김혜수</p>
               <p className="text-xs font-medium text-gray-500">프로필 보기</p>
             </div>
           </div>
-          <div className="mt-5">
-            <h1 className="text-3xl font-bold text-gray-900">강아지 산책</h1>
-            
-            <div className="flex flex-col pt-5">
-              <span className="text-lg text-gray-500">
-                2023년 4월 1일 오전 10시 부터
-              </span>
-              <span className="text-lg text-gray-500">
-                2023년 4월 3일 오후 12시 까지
-              </span>
-              <span className="mt-3 text-[23px] font-medium text-gray-900">
-                200만원
+          <div className="pt-4">
+            <h1 className="text-xl font-bold text-gray-900">강아지 산책</h1>
+            <div className="flex flex-col pt-2" />
+            <div className="flex items-center space-x-3 rounded-lg border border-gray-400 py-10">
+              <span className="px-4 text-sm text-black">
+                강아지 산책 부탁드려요!!
               </span>
             </div>
-            <p className=" my-6 text-gray-700">
-              강아지 산책 좀 해주쇼..
-            </p>
-            <div className="flex items-center justify-between space-x-2">
-              <button className="flex-1 rounded-md bg-blue-500 py-3 font-medium text-white hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2 ">
+            <div className="pt-4" />
+            <div className="flex flex-col space-x-3 rounded-lg border border-gray-400 py-2">
+              <p className="mb-2 px-4 text-xl font-bold">요청시간</p>
+              <span className="px-2 text-sm text-black">
+                2023년 4월 1일 오전 10시 ~ 4월 3일 오후 12시 까지
+              </span>
+            </div>
+            <div className="pt-4" />
+            <div className="flex flex-col space-x-3 rounded-lg border border-gray-400 py-2">
+              <p className="mb-2 px-4 text-xl font-bold">서비스 비용</p>
+              <span className="px-2 text-sm text-black">50000 원</span>
+            </div>
+            <div className="pt-4" />
+            <div className="flex flex-col space-x-3 rounded-lg border border-gray-400 py-2">
+              <p className="mb-2 px-4 text-xl font-bold">서비스 방법</p>
+              <span className="px-2 text-sm text-black">화상통화</span>
+            </div>
+            <div className="flex items-center justify-between space-x-2 pt-16">
+              <button className="flex-1 rounded-md bg-black py-3 font-medium text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-gray-700 focus:ring-offset-1 ">
                 채팅 보내기
               </button>
               <button className="flex items-center justify-center rounded-md p-3 text-gray-400 hover:bg-gray-100 hover:text-gray-500">
@@ -52,20 +59,6 @@ const ServiceDetail: NextPage = () => {
                 </svg>
               </button>
             </div>
-          </div>
-        </div>
-        <div>
-          <h1 className="text-xl font-bold text-gray-900">
-            이 글과 함께 봤어요
-          </h1>
-          <div className=" mt-6 grid grid-cols-2 gap-4">
-            {[1, 2, 3, 4, 5, 6].map((_, i) => (
-              <div key={i}>
-                <div className="mb-4 h-56 w-full bg-slate-300" />
-                <h3 className="-mb-1 text-gray-700">강아지 산책</h3>
-                <span className="text-sm font-medium text-gray-900">20만원</span>
-              </div>
-            ))}
           </div>
         </div>
       </div>
