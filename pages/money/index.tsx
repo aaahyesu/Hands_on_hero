efca23fa85885548ab21b072593051a835e1245d
lee <rrtu123@naver.com>
2023-09-06 23:16:06 +0900

    money api

--- pages/money/index.tsx
+++ pages/money/index.tsx
@@ -1,13 +1,12 @@
 import type { NextPage } from "next";
 import Layout from "@/components/navbar";
-import Link from "next/link";
 import useUser from "@/libs/client/useUser";
 import { useRouter } from "next/router";
 import { useForm } from "react-hook-form";
-import inquiry from "../api/inquiry";
-import { Inquiry, User } from "@prisma/client";
+import { User } from "@prisma/client";
 import useMutation from "@/libs/client/useMutation";
 import Input from "@/components/input";
+import Link from "next/link";
 
 interface virtualAccount {
   virtualAccount: string;
@@ -42,7 +41,7 @@ const Money: NextPage = () => {
           </div>
         </div>
       </div>
-      <div className="flex items-center px-4 py-4">
+      <div className="flex items-center px-4 py-4 pb-40">
         <p className=" mr-4 px-10 text-sm font-semibold text-gray-700">
           충전할 금액
         </p>
@@ -58,12 +57,16 @@ const Money: NextPage = () => {
             />
         </div>
       </div>
-      <button className="px-60 mt-52 rounded-md border-2 border-transparent bg-black py-3 text-center font-medium text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-gray-700 focus:ring-offset-1 ">
+      <div className="flex justify-center gap-4">
+      <button className="px-20 rounded-md border-2 border-transparent bg-black py-3 text-center font-medium text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-gray-700 focus:ring-offset-1">
         충전하기
       </button>
-      <button className="text-blacks px-60 mt-4 rounded-md border-2 border-gray-300 bg-white py-3 text-center font-medium hover:bg-gray-200 focus:outline-none focus:ring-2 focus:ring-gray-700 focus:ring-offset-1 ">
+      <Link href="/mypage">
+      <button className="px-20 rounded-md border-2 border-gray-300 bg-white py-3 text-center font-medium hover:bg-gray-200 focus:outline-none focus:ring-2 focus:ring-gray-700 focus:ring-offset-1">
         취소하기
       </button>
+      </Link>
+      </div>
       </form>
     </Layout>
   );
