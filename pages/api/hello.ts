99c042a6cd0c535f0b9ae2178e8ba2eb80b2178a
Hyesu <skxldzm123@naver.com>
2023-11-09 00:21:23 +0900

    lll

---
+++ pages/api/hello.ts
@@ -0,0 +1,18 @@
+// api > hello.ts
+
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import type { NextApiRequest, NextApiResponse } from "next";
+import { PrismaClient } from "@prisma/client";
+const prisma = new PrismaClient();
+
+type Data = {
+  name: string;
+};
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<Data>
+) {
+  const result: any = await prisma.user.findMany();
+  res.status(200).json(result);
+}
